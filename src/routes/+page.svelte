75527a5e98f3c8e1b51d03f4f9e61e2b464f01f7
Devansh Agarwal <devansh4m@gmail.com>
2025-07-03 15:21:49 +0530

    chore: disabled unused footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -39,7 +39,7 @@
   <SettingsWindow />
   <LiveStatsWindow />
 
-  <footer class="px-5 pt-16 pb-4">
+  <!-- <footer class="px-5 pt-16 pb-4">
     <div class="mx-auto max-w-[40rem]">
       <div aria-hidden="true" class="h-[1px] bg-slate-700"></div>
       <div class="flex items-center justify-between p-2">
@@ -65,7 +65,7 @@
         </a>
       </div>
     </div>
-  </footer>
+  </footer> -->
 </div>
 
 <style lang="postcss">
