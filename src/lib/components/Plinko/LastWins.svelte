79a8d34d2b0bdad4341252971a86e6eea29c8d79
Devansh Agarwal <devansh4m@gmail.com>
2025-07-10 15:59:07 +0530

    refactor: adjust padding and font sizes in LastWins, Sidebar, and Select components for improved visual consistency and user experience

--- src/lib/components/Plinko/LastWins.svelte
+++ src/lib/components/Plinko/LastWins.svelte
@@ -18,7 +18,7 @@
 <div class="flex flex-row gap-1 overflow-hidden">
   {#each lastWins as { binIndex, rowCount, payout: { multiplier } }, index}
     <div
-      class="flex items-center justify-center px-2 py-1.5 text-xs font-bold text-gray-950 {index === 0 ? 'rounded-l-md' : ''} {index === lastWins.length - 1 ? 'rounded-r-md' : ''}"
+      class="flex h-8 w-9 items-center justify-center text-xs font-bold text-gray-950 {index === 0 ? 'rounded-l-md' : ''} {index === lastWins.length - 1 ? 'rounded-r-md' : ''}"
       style:background-color={binColorsByRowCount[rowCount].background[binIndex]}
     >
       {multiplier}{multiplier < 100 ? '×' : ''}
